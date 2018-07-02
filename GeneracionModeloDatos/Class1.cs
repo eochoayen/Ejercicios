4b283452ae64005e1fd498591779c9f1e4f987fe
Erick Ochoa <eochoayen@hotmail.com>
2018-07-01 20:48:03 -0500

    clean

--- GeneracionModeloDatos/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GeneracionModeloDatos
-{
-    public class Class1
-    {
-    }
-}
--- VerificandoDBSqlite.sql
+++
@@ -1,6 +0,0 @@
---verificando sqlite
-
-select * from Listas where ListaId in ('46180748', '46700892', '46700837');
-
-
-select * from DetalleLista;
